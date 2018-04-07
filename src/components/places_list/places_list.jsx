fe68374a5da5ce4f1e991c514637555485b3ead9
igor-ribeiiro <ribeiro_igor@hotmail.com>
2018-04-07 17:35:51 -0300

    Making good changes on return list

--- src/components/places_list/places_list.jsx
+++ src/components/places_list/places_list.jsx
@@ -16,12 +16,25 @@ class PlacesList extends Component {
   getPlacesList(places) {
     const listItems = places.map((place) =>
       <li key={place.name.toString()}>
-        <img src={place.photo} alt="Smiley face"/>
-        <h3>{place.name}</h3>
-        <p>{place.address}</p>
-
+        <div className="media">
+          <div className="fav-box"><i className="fa fa-heart-o" aria-hidden="true"></i>
+          </div>
+          <img className="d-flex align-self-start" src={place.photo} alt="Gatinho!"/>
+            <div className="media-body pl-3">
+              <div className="price">{place.name}<small>{place.address}</small></div>
+              <div className="stats">
+                <span><i className="fa fa-arrows-alt"></i>1678 Sq ft</span>
+                <span><i className="fa fa-bath"></i>2 Beadrooms</span>
+              </div>
+              <div className="address">4062 Walnut Hill Drive
+                Cincinnati</div>
+            </div>
+        </div>
       </li>
     );
+
+
+
     return (
       <ul>{listItems}</ul>
     );
@@ -43,12 +56,31 @@ class PlacesList extends Component {
         "name": "name3",
         "address": "rua 3",
         "photo": img3
+      },
+      {
+        "name": "name4",
+        "address": "rua 4",
+        "photo": img1
       }
     ];
 
     return (
       <div className="PlacesList">
-        {this.getPlacesList(places)}
+        <section className="search-box">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-7 map-box mx-0 px-0">
+                <iframe width="100%" height="495" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"></iframe>
+              </div>
+              <div className="col-md-12 listing-block">
+                <ul>
+                  {this.getPlacesList(places)}
+                </ul>
+              </div>
+
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
