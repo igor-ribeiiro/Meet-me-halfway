4bd6911ae3c8a177ceaf6445754ca92e388676be
HikkusT <luizhenrique98@gmail.com>
2018-04-07 16:34:32 -0300

    Fixed small issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Input from './components/input.jsx'
+import Input from './components/input/input.jsx'
 import SearchBar from './components/SearchBar/SearchBar.jsx';
 import PersonAddress from './components/PersonAddress/PersonAddress.jsx';
 import Header from './components/header/header.jsx'
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import Input from '../input/input.jsx'
 import Map from '../map/map.jsx'
 import PlacesList from "../places_list/places_list";
+import PersonAddress from '../PersonAddress/PersonAddress';
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-        <Input/>
+        <PersonAddress/>
         <Map/>
         <PlacesList numbers={[1, 2, 3, 4, 5]}/>
       </div>
