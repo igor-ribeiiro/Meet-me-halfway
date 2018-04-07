f2fa94461980176f33e9f023d3b84789e2ef025c
igor-ribeiiro <ribeiro_igor@hotmail.com>
2018-04-07 15:16:26 -0300

    Adding map class

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -2,19 +2,6 @@ import React, { Component } from 'react';
 import "./map.css"
 
 class Map extends Component {
-
-  initMap() {
-    let uluru = {lat: -25.363, lng: 131.044};
-    let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    let marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
-
   render() {
     return (
       <div>
