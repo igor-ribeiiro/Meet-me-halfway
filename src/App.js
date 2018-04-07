8f9814887fa87eb9eca49ded713089ca4ee5af0c
igor-ribeiiro <ribeiro_igor@hotmail.com>
2018-04-07 14:36:32 -0300

    Adding macaco class!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from './components/input.jsx'
 
 class App extends Component {
+
+
+
   render() {
-    var a = 2;
+    let a = 2;
     return (
       <div className="App">
+          <Input/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/components/input.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+    render() {
+        return (
+            <p>On input class</p>
+        );
+    }
+}
+
+export default Input;
