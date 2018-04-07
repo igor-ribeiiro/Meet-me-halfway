9a58ad59e205ec0de0e11bdf3d5c76687a00af62
igor-ribeiiro <ribeiro_igor@hotmail.com>
2018-04-07 15:25:54 -0300

    Adding header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header.jsx'
 import Input from './components/input/input.jsx'
 import Map from './components/map/map.jsx'
 
 class App extends Component {
-
-
-
   render() {
     return (
       <div className="App">
+        <Header/>
           <Input/>
         <Map/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code>  and save to reload.
-        </p>
+
       </div>
     );
   }
