4c2d74687080719a302db94e3be403d825a64ce3
HikkusT <luizhenrique98@gmail.com>
2018-04-07 19:30:18 -0300

    Added calculate button. Also addeda wrapper for the addresses

---
+++ src/components/AddressContainer/AddressContainer.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import SearchBar from '../SearchBar/SearchBar';
+
+class AddressContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "coord" : []
+        };
+    
+         this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(lat, lng, i) {
+        this.props.handleInput(lat, lng, i);
+        //window.globalPos.push({
+            //lat: -23.59,
+            //lng: -46.68
+        //});
+        //console.log(window.globalPos[0]);
+    }
+    
+    render(){
+        return <SearchBar index = {this.props.index} handleInput= {this.handleInput} />
+    }
+}
+
+export default AddressContainer;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import PlacesList from "../places_list/places_list";
-import PersonAddress from '../PersonAddress/PersonAddress';
+import AddressWrapper from '../AddressWrapper/AddressWrapper';
 import '../../App.css';
 
 class Main extends Component {
+  Calculate(coords){
+    console.log(coords.length);
+    if (coords.length < 4)
+      alert("Missing address!");
+    else
+      alert("Calculate Here!");
+  }
+
   render() {
     return (
       <div className="Main">
-        <PersonAddress/>
+        <AddressWrapper onClick = {this.Calculate}/>
         <PlacesList/>
       </div>
     );
